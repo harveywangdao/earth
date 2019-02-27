700549b65359ba50226c422a889e71079baae7cc
王道安 <wangdaoan@m-chain.com>
2019-02-27 16:19:34 +0800

    defer

--- elegant/defer2.go
+++ elegant/defer2.go
@@ -22,7 +22,37 @@ func add2() int {
 	return sum + 4
 }
 
+func add3() (sum int) {
+	sum = 1
+	defer func() {
+		sum = 3
+	}()
+
+	return 10
+}
+
+func add4() (sum int) {
+	sum = 1
+	defer func() {
+		sum = 4
+	}()
+
+	return sum
+}
+
+func add5() (sum int) {
+	sum = 1
+	defer func() {
+		sum = 5
+	}()
+
+	return sum + 70
+}
+
 func main() {
-	fmt.Println(add())
-	fmt.Println(add2())
+	fmt.Println(add())  //2
+	fmt.Println(add2()) //5
+	fmt.Println(add3()) //3
+	fmt.Println(add4()) //4
+	fmt.Println(add5()) //5
 }
