067f2dac9d701a55e270823bc18050bf9134a710
thomas <1272777053@qq.com>
2019-04-28 22:25:19 +0800

    cond

--- paper/cond.c
+++ paper/cond.c
@@ -48,23 +48,15 @@ void *task3(void *arg)
   int ret;
   while(running)
   {
-    pthread_mutex_lock(&mutex);
-
     printf("task3 pthread_cond_signal start\n");
     pthread_cond_signal(&cond);
     printf("task3 pthread_cond_signal end\n");
 
-    pthread_mutex_unlock(&mutex);
-
     sleep(1);
-
-    pthread_mutex_lock(&mutex);
 
     printf("task3 pthread_cond_broadcast start\n");
     pthread_cond_broadcast(&cond);
     printf("task3 pthread_cond_broadcast end\n");
-
-    pthread_mutex_unlock(&mutex);
 
     sleep(1);
   }
@@ -108,9 +100,124 @@ void do1()
 
   running = 0;
 
-  pthread_mutex_lock(&mutex);
+  sleep(3);
+
   pthread_cond_broadcast(&cond);
-  pthread_mutex_unlock(&mutex);
+
+  pthread_join(pt1, NULL);
+  pthread_join(pt2, NULL);
+  pthread_join(pt3, NULL);
+
+  pthread_cond_destroy(&cond);
+  pthread_mutex_destroy(&mutex);
+}
+
+void *taska(void *arg)
+{
+  while(running)
+  {
+    pthread_mutex_lock(&mutex);
+
+    pthread_cond_wait(&cond, &mutex);
+
+    printf("taska count:%d\n", count);
+
+    pthread_mutex_unlock(&mutex);
+  }
+
+  printf("taska exit\n");
+}
+
+void *taskb(void *arg)
+{
+  while(running)
+  {
+    pthread_mutex_lock(&mutex);
+
+    pthread_cond_wait(&cond, &mutex);
+
+    printf("taskb count:%d\n", count);
+
+    pthread_mutex_unlock(&mutex);
+  }
+  
+  printf("taskb exit\n");
+}
+
+void *taskc(void *arg)
+{
+  int broadcast;
+
+  while(running)
+  {
+    broadcast = 0;
+
+    pthread_mutex_lock(&mutex);
+    count++;
+
+    if (count % 17 == 0)
+    {
+      broadcast = count / 17;
+    }
+
+    pthread_mutex_unlock(&mutex);
+
+    if (broadcast)
+    {
+      if (broadcast % 2 == 0)
+      {
+        pthread_cond_broadcast(&cond);
+      }
+      else
+      {
+        pthread_cond_signal(&cond);
+      }
+    }
+
+    usleep(10000);
+  }
+
+  printf("taskc exit\n");
+}
+
+void do2()
+{
+  pthread_t pt1;
+  pthread_t pt2;
+  pthread_t pt3;
+  int ret = -1;
+
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&cond, NULL);
+
+  ret = pthread_create(&pt1, NULL, (void*)taska, NULL);
+  if (ret != 0)
+  {
+    printf("create thread fail\n");
+    return;
+  }
+
+  ret = pthread_create(&pt2, NULL, (void*)taskb, NULL);
+  if (ret != 0)
+  {
+    printf("create thread fail\n");
+    return;
+  }
+
+  ret = pthread_create(&pt3, NULL, (void*)taskc, NULL);
+  if (ret != 0)
+  {
+    printf("create thread fail\n");
+    return;
+  }
+
+  sleep(4);
+
+  running = 0;
+
+  sleep(3);
+
+  pthread_cond_broadcast(&cond);
 
   pthread_join(pt1, NULL);
   pthread_join(pt2, NULL);
@@ -122,7 +229,8 @@ void do1()
 
 int main(int argc, char const *argv[])
 {
-	do1();
+  //do1();
+	do2();
 
 	return 0;
 }
