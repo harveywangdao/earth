8251433a608b774c81d090b23aa8414421569691
thomas <1272777053@qq.com>
2019-04-29 23:17:41 +0800

    mmap

--- paper/mmap.c
+++ paper/mmap.c
@@ -89,8 +89,88 @@ void do1()
   remove("file.txt");
 }
 
+void do2()
+{
+  int ret;
+  int mapsize = 32;
+
+  char *addr = mmap(0, mapsize, PROT_READ | PROT_WRITE, MAP_SHARED|MAP_ANON, -1, 0);
+  if (addr == MAP_FAILED)
+  {
+    perror("mmap fail");
+    return;
+  }
+
+  pid_t pid;
+  pid = fork();
+  if (pid == -1)
+  {
+    printf("fork fail\n");
+    return;
+  }
+  else if (pid == 0)
+  {
+    printf("son start, pid = %d, ppid = %d\n", getpid(), getppid());
+
+    addr[0] = 'a';
+    addr[1] = 'b';
+    addr[2] = 'c';
+    addr[3] = 'd';
+
+    printf("son addr:%s\n", addr);
+
+    ret = msync(addr, mapsize, MS_SYNC);
+    if (ret == -1)
+    {
+      perror("msync fail");
+      return;
+    }
+    
+    ret = munmap(addr, mapsize);
+    if (ret == -1)
+    {
+      perror("munmap fail");
+      return;
+    }
+
+    printf("son end, pid = %d, ppid = %d\n", getpid(), getppid());
+    exit(0);
+  }
+  else
+  {
+    sleep(2);
+    printf("addr:%s\n", addr);
+
+    ret = msync(addr, mapsize, MS_SYNC);
+    if (ret == -1)
+    {
+      perror("msync fail");
+      return;
+    }
+    
+    ret = munmap(addr, mapsize);
+    if (ret == -1)
+    {
+      perror("munmap fail");
+      return;
+    }
+
+    int status = 0;
+    int ret = waitpid(pid, &status, 0);
+    if (ret == -1)
+    {
+      printf("son ret = %d, status = %d\n", ret, status);
+      return;
+    }
+
+    printf("son ret = %d, status = %d\n", ret, status);
+  }
+}
+
 int main(int argc, char const *argv[])
 {
-	do1();
+  //do1();
+	do2();
+
 	return 0;
 }
