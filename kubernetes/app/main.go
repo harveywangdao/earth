04500887b00d016ec35bfc7b35be072f278f013b
thomas <1272777053@qq.com>
2020-08-17 00:35:12 +0800

    rbac

--- kubernetes/app/main.go
+++ kubernetes/app/main.go
@@ -1,12 +1,72 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	var dir, dir2 string
+	flag.StringVar(&dir, "dir", "/data/pear", "dir")
+	flag.StringVar(&dir2, "dir2", "/data/pear_pvc", "dir2")
+	flag.Parse()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		err := os.MkdirAll(dir, 0777)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		f, err := os.OpenFile(filepath.Join(dir, "data.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		defer f.Close()
+
+		_, err = f.WriteString(time.Now().Format("Mon Jan 2 15:04:05 -0700 MST 2006") + "\n")
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		err = os.MkdirAll(dir2, 0777)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		f2, err := os.OpenFile(filepath.Join(dir2, "data.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		defer f2.Close()
+
+		_, err = f2.WriteString(time.Now().Format("Mon Jan 2 15:04:05 -0700 MST 2006") + "\n")
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
 		c.JSON(200, gin.H{
 			"message": "today is yesterday",
 		})
