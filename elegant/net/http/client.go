2a0df24e34aad419ca2e171faf7ad05e68025256
thomas <sky@qq.com>
2021-07-12 14:42:05 +0800

    s

--- elegant/net/http/client.go
+++ elegant/net/http/client.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func do1() {
@@ -35,7 +37,137 @@ func do1() {
 	}
 }
 
+func do2() {
+	buf := bytes.NewBufferString("hello, I am http client")
+	req, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8564/api", buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Connection", "keep-alive")
+	//req.Header.Set("Connection", "close")
+
+	cli := &http.Client{}
+	resp, err := cli.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer resp.Body.Close()
+
+	log.Println(resp.Header)
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("http client recv msg: %s", data)
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("http code:", resp.Status)
+	}
+
+	resp.Body.Close()
+	time.Sleep(time.Hour)
+}
+
+func do3() {
+	cli := &http.Client{}
+
+	for {
+		buf := bytes.NewBufferString("hello, I am http client")
+		req, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8564/api", buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//req.Header.Set("Connection", "keep-alive")
+		req.Header.Set("Connection", "close")
+
+		resp, err := cli.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(resp.Header)
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("http client recv msg: %s", data)
+
+		if resp.StatusCode != http.StatusOK {
+			log.Fatal("http code:", resp.Status)
+		}
+		resp.Body.Close()
+
+		time.Sleep(time.Second)
+	}
+}
+
+func do4() {
+	bufr, bufw := io.Pipe()
+
+	go func() {
+		bufw.Write([]byte("orange1"))
+		time.Sleep(time.Second)
+		bufw.Write([]byte("orange2"))
+		time.Sleep(time.Second)
+		bufw.Write([]byte("orange3"))
+		time.Sleep(time.Second)
+		bufw.Write([]byte("orange4"))
+		time.Sleep(time.Second)
+		bufw.Write([]byte("orange5"))
+		bufw.Close()
+	}()
+
+	req, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8564/chunk", bufr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//req.Header.Set("Transfer-Encoding", "chunked")
+
+	log.Println("send http")
+
+	cli := &http.Client{}
+	resp, err := cli.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	log.Println(resp.Header)
+
+	buf := make([]byte, 128)
+	for {
+		n, err := resp.Body.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				if n > 0 {
+					log.Printf("http client recv msg: %s", buf[:n])
+				}
+				break
+			}
+			log.Fatal(err)
+		}
+		log.Printf("http client recv msg: %s", buf[:n])
+	}
+
+	/*	data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("http client recv msg: %s", data)*/
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("http code:", resp.Status)
+	}
+}
+
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	do1()
+	do4()
 }
--- elegant/net/http/server.go
+++ elegant/net/http/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func do1() {
@@ -23,7 +25,71 @@ func do1() {
 	log.Fatal(http.ListenAndServe(":8564", mux))
 }
 
+func do2() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Header)
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("http server recv msg: %s", data)
+
+		//w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Connection", "close")
+		w.Write([]byte("hello, I am http server"))
+	})
+
+	log.Println("http server listen:", "8564")
+	log.Fatal(http.ListenAndServe(":8564", mux))
+}
+
+func do3() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/chunk", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Header)
+
+		buf := make([]byte, 128)
+		for {
+			n, err := r.Body.Read(buf)
+			if err != nil {
+				if err == io.EOF {
+					if n > 0 {
+						log.Printf("http server recv msg: %s", buf[:n])
+					}
+					break
+				}
+				log.Fatal(err)
+			}
+			log.Printf("http server recv msg: %s", buf[:n])
+		}
+
+		//w.Header().Set("Transfer-Encoding", "chunked")
+		w.Write([]byte("apple1"))
+		w.(http.Flusher).Flush()
+		time.Sleep(time.Second)
+
+		w.Write([]byte("apple2"))
+		w.(http.Flusher).Flush()
+		time.Sleep(time.Second)
+
+		w.Write([]byte("apple3"))
+		w.(http.Flusher).Flush()
+		time.Sleep(time.Second)
+
+		w.Write([]byte("apple4"))
+		w.(http.Flusher).Flush()
+		time.Sleep(time.Second)
+
+		w.Write([]byte("apple5"))
+		w.(http.Flusher).Flush()
+	})
+
+	log.Println("http server listen:", "8564")
+	log.Fatal(http.ListenAndServe(":8564", mux))
+}
+
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	do1()
+	do3()
 }
