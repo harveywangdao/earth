fe7cf8f56f57d36ecb8e2c9c57deb2be4b336126
thomas <1272777053@qq.com>
2022-05-16 23:29:36 +0800

    d

--- c/signal.c
+++ c/signal.c
@@ -384,8 +384,22 @@ void do12()
 	while(1);
 }
 
+/*
+struct sigaction {
+	void     (*sa_handler)(int);
+	void     (*sa_sigaction)(int, siginfo_t *, void *);
+	sigset_t   sa_mask;
+	int        sa_flags;
+	void     (*sa_restorer)(void);
+};
+*/
+void do13()
+{
+	sigaction();
+}
+
 int main(int argc, char const *argv[])
 {
-	do12();
+	do13();
 	return 0;
 }
