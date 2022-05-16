c4cba9d8888996e013bf7d65358a6ac8929774c9
thomas <sky@qq.com>
2022-05-16 21:48:57 +0800

    d

--- c/signal.c
+++ c/signal.c
@@ -299,8 +299,93 @@ void do10()
 	while(1);
 }
 
+void do11()
+{
+	struct timespec ts;
+	int ret1 = clock_getres(CLOCK_REALTIME, &ts);
+	printf("clock_getres ret: %d\n", ret1);
+	printf("clock_getres tv_sec: %ld\n", ts.tv_sec);
+	printf("clock_getres tv_nsec: %ld\n", ts.tv_nsec);
+
+	int ret2 = clock_gettime(CLOCK_REALTIME, &ts);
+	printf("clock_gettime ret: %d\n", ret2);
+	printf("clock_gettime tv_sec: %ld\n", ts.tv_sec);
+	printf("clock_gettime tv_nsec: %ld\n", ts.tv_nsec);
+
+	ts.tv_sec++;
+	int ret3 = clock_settime(CLOCK_REALTIME, &ts);
+	printf("clock_settime ret: %d\n", ret3);
+	perror("clock_settime fail");
+}
+
+       
+static void print_siginfo(siginfo_t *si)
+{
+	timer_t *tidp;
+	int or;
+
+	tidp = si->si_value.sival_ptr;
+
+	printf("    sival_ptr = %p; ", si->si_value.sival_ptr);
+	printf("    *sival_ptr = 0x%lx\n", (long) *tidp);
+
+	or = timer_getoverrun(*tidp);
+	if (or == -1)
+		handle_error("timer_getoverrun");
+	else
+		printf("    overrun count = %d\n", or);
+}
+
+static void handler(int sig, siginfo_t *si, void *uc)
+{
+	printf("caught signal %d\n", sig);
+	print_siginfo(si);
+	signal(sig, SIG_IGN);
+}
+
+// gcc -o app signal.c -lrt
+// /proc/[pid]/timers
+void do12()
+{
+	struct sigevent sev;
+	timer_t timerid;
+	struct itimerspec its;
+	long long freq_nanosecs = 100;
+	sigset_t mask;
+	struct sigaction sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		handle_error("sigaction");
+
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGUSR1);
+	if (sigprocmask(SIG_SETMASK, &mask, NULL) == -1)
+		handle_error("sigprocmask");
+
+	sev.sigev_notify = SIGEV_SIGNAL;
+	sev.sigev_signo = SIGUSR1;
+	sev.sigev_value.sival_ptr = &timerid;
+	if (timer_create(CLOCK_REALTIME, &sev, &timerid) == -1)
+		handle_error("timer_create");
+
+	printf("timer_create timerid: %ld\n", (long)timerid);
+
+	its.it_value.tv_sec = freq_nanosecs / 1000000000;
+	its.it_value.tv_nsec = freq_nanosecs % 1000000000;
+	its.it_interval.tv_sec = its.it_value.tv_sec;
+	its.it_interval.tv_nsec = its.it_value.tv_nsec;
+
+	if (timer_settime(timerid, 0, &its, NULL) == -1)
+		handle_error("timer_settime");
+
+	while(1);
+}
+
 int main(int argc, char const *argv[])
 {
-	do10();
+	do12();
 	return 0;
 }
