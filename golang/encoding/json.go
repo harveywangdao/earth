0a64e206cea0152fcd84211b198b82720c66c25e
thomas <sky@qq.com>
2022-11-03 18:23:16 +0800

    d

--- golang/encoding/json.go
+++ golang/encoding/json.go
@@ -26,9 +26,7 @@ func do1() {
 	fmt.Println(add(1, 2))
 }
 
-func main() {
-	do1()
-	return
+func do2() {
 	a := People{
 		Name: "xiaoming",
 		Age:  12,
@@ -48,4 +46,44 @@ func main() {
 		fmt.Println("m:", m)
 		time.Sleep(1 * time.Second)
 	}
+}
+
+// 字节切片被转成base64
+func do3() {
+	p := &People{
+		Name: "xiaoming",
+		Age:  12,
+	}
+	data, _ := json.Marshal(p)
+
+	m := make(map[string]interface{})
+	m["data1"] = string(data)
+	m["data2"] = data
+
+	data2, _ := json.Marshal(m)
+	fmt.Println(string(data2))
+
+	m2 := make(map[string]interface{})
+	json.Unmarshal(data2, &m2)
+	fmt.Println(m2)
+
+	type Human struct {
+		Data1 string `json:"data1"`
+		Data2 []byte `json:"data2"`
+	}
+	h1 := Human{}
+	json.Unmarshal(data2, &h1)
+	fmt.Println(h1.Data1, string(h1.Data2))
+
+	type Human2 struct {
+		Data1 string `json:"data1"`
+		Data2 string `json:"data2"`
+	}
+	h2 := Human2{}
+	json.Unmarshal(data2, &h2)
+	fmt.Println(h2.Data1, h2.Data2)
+}
+
+func main() {
+	do3()
 }
---
+++ golang/net/http/web/go.mod
@@ -0,0 +1,5 @@
+module github.com/harveywangdao/earth/golang/net/http/web
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.8.1 // indirect
