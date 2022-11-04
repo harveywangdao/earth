ff6d7651e4c478a0e85a614cafd5a093afccd6fa
thomas <sky@qq.com>
2022-11-04 10:47:07 +0800

    d

--- golang/encoding/toml.go
+++ golang/encoding/toml.go
@@ -24,6 +24,45 @@ Name = "xiaoming2"
 	fmt.Println(p)
 }
 
+/*
+[Notify]
+  AccessKey = "123"
+  EnableV2Notification = true
+  [Notify.TemplatesV2]
+    file_comment_replied = "53"
+    file_commented = "54"
+    mentioned_in_doc = "55"
+*/
+type conf struct {
+	Notify *NotifyConfig
+}
+
+type NotifyConfig struct {
+	AccessKey            string
+	EnableV2Notification bool
+	TemplatesV2          map[string]string
+}
+
+func do2() {
+	n := &NotifyConfig{
+		AccessKey:            "123",
+		EnableV2Notification: true,
+	}
+	t := make(map[string]string)
+	t["file_comment_replied"] = "53"
+	t["file_commented"] = "54"
+	t["mentioned_in_doc"] = "55"
+	n.TemplatesV2 = t
+
+	c := &conf{
+		Notify: n,
+	}
+
+	if err := toml.NewEncoder(os.Stdout).Encode(c); err != nil {
+		fmt.Println("Error encoding TOML: %s", err)
+	}
+}
+
 func main() {
-	do1()
+	do2()
 }
