92fc3ca029d32592aaa221e00c2592b5d95e3607
harveywangdao <1272777053@qq.com>
2022-11-28 09:46:21 +0000

    d

--- cpp/static.cpp
+++ cpp/static.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 void do1()
 {
-  //static_assert(false);
+  static_assert(true); // c++17
   static_assert(true, "this is true");
-  //static_assert(false, "this is false");
 
   const int a = 3;
   const int b = 3;
@@ -15,14 +14,18 @@ void do1()
   static_assert(a == b, "a == b");
 }
 
+// g++ -std=c++17 -o app static.cpp 
 void do2()
 {
   int a = 10;
-  static_assert(is_same_v<int,decltype(a)>, "type diff");
+  int &b = a;
+  static_assert(is_same_v<int,decltype(a)>, "decltype(a) is not int");
+  static_assert(is_same_v<int&,decltype(b)>, "decltype(b) is not int&");
+  static_assert(is_same_v<int&,decltype(a)>, "decltype(a) is not int&");
 }
 
 int main(int argc, char const *argv[])
 {
-  do2();
+  do1();
   return 0;
 }
