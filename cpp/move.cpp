2b610d9a7b9453c51c1e727990973447741f6c63
harveywangdao <1272777053@qq.com>
2022-12-05 10:21:48 +0000

    f

--- cpp/move.cpp
+++ cpp/move.cpp
@@ -34,16 +34,21 @@ void do1()
   std::cout << "y4 = " << y4 << std::endl;
   std::cout << "y5 = " << y5 << std::endl;
   std::cout << "y6 = " << y6 << std::endl;
-
 }
 
 void do2()
 {
   int a = 10;
-  int b = move(a);
+  int b = std::move(a);
+  int&& c = 20;
+
   a++;
   b++;
-  cout << "a: " << a << ", b: " << b << endl;
+  c++;
+
+  std::cout << "a: " << a << std::endl;
+  std::cout << "b: " << b << std::endl;
+  std::cout << "c: " << c << std::endl;
 }
 
 void do3()
@@ -66,60 +71,116 @@ void do3()
 
 class People
 {
-public:
   int size;
-  int *data;
 
-  People()
+public:
+  People():size(0)
   {
-    size = 0;
-    data = nullptr;
-    cout << "default" << endl;
+    cout << "default" << ", size: " << size << ", this: " << this << endl;
   }
 
-  People(int sz)
+  //explicit People(int sz):size(sz)
+  People(int sz):size(sz)
   {
-    size = sz;
-    data = new int[size];
-    cout << "with size" << endl;
+    cout << "with size" << ", size: " << size << ", this: " << this << endl;
   }
 
-  People(const People &p)
+  //explicit People(const People& p):size(p.size)
+  People(const People& p):size(p.size)
+  {
+    cout << "left ref" << ", size: " << size << ", this: " << this << endl;
+  }
+
+  //explicit People(People&& p):size(std::move(p.size))
+  People(People&& p):size(std::move(p.size))
+  {
+    cout << "right ref" << ", size: " << size << ", this: " << this << endl;
+  }
+
+  People& operator=(const People& p)
   {
     size = p.size;
-    data = new int[p.size];
-    cout << "left ref" << endl;
+    cout << "operator= &" << ", size: " << size << ", this: " << this << endl;
+    return *this;
   }
 
-  People(People &&p)
+  People& operator=(People&& p)
   {
-    size = p.size;
-    data = p.data;
-    p.data = nullptr;
-    cout << "right ref" << endl;
+    size = std::move(p.size);
+    cout << "operator= &&" << ", size: " << size << ", this: " << this << endl;
+    return *this;
   }
-
-  /*People& operator=(const People &p)
-  {
-    size = p.size;
-    data = p.data;
-    cout << "operator=" << endl;
-  }*/
 
   virtual ~People()
   {
-    if (data != nullptr)
-    {
-      delete[] data;
-      data = nullptr;
-    }
-    cout << "People destroy" << endl;
+    cout << "People destroy" << ", size: " << size << ", this: " << this << endl;
   }
 };
 
-// g++ -std=c++14 -o app rightref.cpp -w -fno-elide-constructors
+void do4()
+{
+  People p1;
+  People p2(2);
+  People p3(p1);
+  People p4(std::move(p1));
+
+  People p5;
+  p5 = p2;
+
+  People p6;
+  p6 = std::move(p2);
+
+  People p7 = 7;
+  People p8 = p7;
+  People p9 = std::move(p7);
+}
+
+People m1()
+{
+  People temp(10);
+  std::cout << "&temp: " << &temp << std::endl;
+  return temp;
+}
+
+/*People& m2()
+{
+  People temp(20);
+  std::cout << "&temp: " << &temp << std::endl;
+  return temp;
+}*/
+
+/*People&& m3()
+{
+  People temp(30);
+  std::cout << "&temp: " << &temp << std::endl;
+  //return temp;
+  return std::move(temp);
+}*/
+
+void do5()
+{
+  {
+    People p1 = m1();
+    std::cout << "&p1: " << &p1 << std::endl; 
+  }
+
+  /*{
+    std::cout << std::endl;
+    People p2 = m2();
+    std::cout << "&p2: " << &p2 << std::endl;
+  }*/
+
+  /*{
+    std::cout << std::endl;
+    People p3 = m3();
+    std::cout << "&p3: " << &p3 << std::endl; 
+  }*/
+}
+
+// g++ -std=c++17 -o app move.cpp -w -fno-elide-constructors
+// g++ -std=c++17 -o app move.cpp
 int main(int argc, char const *argv[])
 {
-  do1();
+  do5();
   return 0;
 }
