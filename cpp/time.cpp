e75abd1740f145a0bc20147385031c88d6cbe59a
thomas <1272777053@qq.com>
2022-12-08 23:54:29 +0800

    f

--- cpp/time.cpp
+++ cpp/time.cpp
@@ -5,6 +5,9 @@
 #include <iomanip>
 #include <ctime>
 #include <thread>
+#include <vector>
+#include <numeric>
+#include <ctime>
 
 void do1()
 {
@@ -36,29 +39,74 @@ using namespace std::literals;
 
 void do3()
 {
-  std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
-  std::time_t t_c = std::chrono::system_clock::to_time_t(now - 24h);
-  std::time_t oldt = std::time(nullptr);
+  auto start = std::chrono::steady_clock::now();
+  std::this_thread::sleep_for(2700ms);
+  auto end = std::chrono::steady_clock::now();
+  auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << diff.count() << "µs ≈ " << (end - start) / 1ms << "ms ≈ " << (end - start) / 1s << "s" << std::endl;
+}
+
+using namespace std::chrono_literals;
+
+void do4()
+{
+  auto t1 = std::chrono::system_clock::now();  
+  std::time_t tt1 = std::chrono::system_clock::to_time_t(t1);
+
   std::this_thread::sleep_for(2700ms);
 
-  std::time_t newt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-  std::cout << "oldt-newt == " << oldt-newt << " s\n";
+  auto t2 = std::chrono::system_clock::now();  
+  std::time_t tt2 = std::chrono::system_clock::to_time_t(t2);
 
-  std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
-/*  std::cout << "Different clocks are not comparable: \n"
-                << " System time: " << now.time_since_epoch() << "\n"
-                << " Steady time: " << start.time_since_epoch() << "\n";*/
+  std::cout << "tt2-tt1 == " << tt2-tt1 << "s" << std::endl;
+  
+  std::time_t t3 = std::time(nullptr);
+  auto from = std::chrono::system_clock::from_time_t(t3);
+  
+  std::this_thread::sleep_for(500ms);
+  
+  auto diff = std::chrono::system_clock::now() - from;
+  
+  //std::cout << diff.count() << " (" << std::chrono::round<std::chrono::milliseconds>(diff).count() << ")" << std::endl;
+}
 
-  const auto end = std::chrono::steady_clock::now();
-      std::cout
-        << "Slow calculations took "
-        << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
-        << (end - start) / 1ms << "ms ≈ " // almost equivalent form of the above, but
-        << (end - start) / 1s << "s.\n";  // using milliseconds and seconds accordingly
+void do5()
+{
+  auto start = std::chrono::steady_clock::now();
+  std::vector<int> v(20000000, 42);
+  std::accumulate(v.begin(), v.end(), 0u);
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  std::cout << "Time to fill and iterate a vector of " << std::setw(9) << " ints : " << diff.count() << " s\n";
+}
+
+void do6()
+{
+  auto start = std::chrono::high_resolution_clock::now();
+  std::vector<int> v(20000000, 42);
+  std::accumulate(v.begin(), v.end(), 0u);
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  std::cout << "Time to fill and iterate a vector of " << std::setw(9) << " ints : " << diff.count() << " s\n";
+}
+
+void do7()
+{
+  const auto p0 = std::chrono::time_point<std::chrono::system_clock>{};
+  const auto p1 = std::chrono::system_clock::now();
+  const auto p2 = p1 - std::chrono::hours(24);
+  
+  std::time_t epoch_time = std::chrono::system_clock::to_time_t(p0);
+  std::cout << "epoch: " << std::ctime(&epoch_time);
+  std::time_t today_time = std::chrono::system_clock::to_time_t(p1);
+  std::cout << "today: " << std::ctime(&today_time);
+  
+  std::cout << "hours since epoch: " << std::chrono::duration_cast<std::chrono::hours>(p1.time_since_epoch()).count() << std::endl;
+  std::cout << "yesterday, hours since epoch: " << std::chrono::duration_cast<std::chrono::hours>(p2.time_since_epoch()).count() << std::endl;
 }
 
 int main(int argc, char const *argv[])
 {
-  do3();
+  do7();
   return 0;
 }
