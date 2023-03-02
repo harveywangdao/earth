90182b911e156e9479ca96efa72c78a0b069ee13
Thomas <1272777053@qq.com>
2023-03-02 20:12:20 +0800

    Update syslog.go

--- golang/logger/syslog.go
+++ golang/logger/syslog.go
@@ -6,11 +6,38 @@ import (
 )
 
 // /var/log/syslog
-func main() {
-	sysLog, err := syslog.Dial("", "", syslog.LOG_ERR, "Saturday")
+func do1() {
+	sysLog, err := syslog.Dial("", "", syslog.LOG_WARNING, "Saturday")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sysLog.Emerg("Hello world!")
+	//sysLog.Emerg("Emerg msg")
+	sysLog.Alert("Alert msg")
+	sysLog.Crit("Crit msg")
+	sysLog.Err("Error msg")
+	sysLog.Warning("Warning msg")
+	sysLog.Notice("Notice msg")
+	sysLog.Info("Info msg")
+	sysLog.Debug("Debug msg")
+}
+
+func do2() {
+	sysLog, err := syslog.Dial("unixgram", "/tmp/xiaom", syslog.LOG_WARNING, "Saturday")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//sysLog.Emerg("Emerg msg")
+	sysLog.Alert("Alert msg")
+	sysLog.Crit("Crit msg")
+	sysLog.Err("Error msg")
+	sysLog.Warning("Warning msg")
+	sysLog.Notice("Notice msg")
+	sysLog.Info("Info msg")
+	sysLog.Debug("Debug msg")
+}
+
+func main() {
+	do2()
 }
