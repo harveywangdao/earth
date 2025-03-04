762a5797981d76c5a2fff608ce209c77dbe49a17
harveywangdao <1272777053@qq.com>
2025-03-04 09:30:23 +0000

    jemalloc

--- node/src/timer.js
+++ node/src/timer.js
@@ -1,9 +1,13 @@
 setTimeout(() => {
     console.log('Timeout callback');
-}, 0);
+}, 1000);
 
 setImmediate(() => {
     console.log('Immediate callback');
 });
 
-console.log('Main thread execution');
+setInterval(() => {
+    console.log('Interval callback');
+}, 1000);
+
+console.log('Main thread');
